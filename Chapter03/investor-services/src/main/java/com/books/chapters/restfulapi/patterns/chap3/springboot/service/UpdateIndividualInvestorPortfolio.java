82b0c72ad00f91d6916871336801969ce54fb5f3
mouad <aassou.mouad@gmail.com>
2020-03-14 22:42:38 +0100

    creating a functional interface for InverstorPortfolio update, using the execute-around pattern

---
+++ Chapter03/investor-services/src/main/java/com/books/chapters/restfulapi/patterns/chap3/springboot/service/UpdateIndividualInvestorPortfolio.java
@@ -0,0 +1,7 @@
+package com.books.chapters.restfulapi.patterns.chap3.springboot.service;
+
+@FunctionalInterface
+public interface UpdateIndividualInvestorPortfolio {
+
+  void updateIndividualInvestorPortfolio();
+}
